3896dc59cef7f482cf26eb2f940e7e6824503b59
Rohit Bhargava <rohitbhargava586@gmail.com>
2017-08-15 18:10:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ element.innerHTML = 'New Value';
 var image = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft = marginLeft - 10;
+    marginLeft = marginLeft + 10;
     image.style.marginLeft = marginLeft + 'px';
 }
 image.onclick = function() {
