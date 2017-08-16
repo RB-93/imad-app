face665e89fb02ef3cc93baa727232e30d285e03
Rohit Bhargava <rohitbhargava586@gmail.com>
2017-08-17 01:32:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,7 +41,8 @@ submit.onclick = function (){
             // Take some action
             if(request.status === 200) {
                 // Capture a list of names and render it as a list.
-                var names = ['name1', 'name2', 'name3', 'name4'];
+                var names = request.responseText;
+                names = JSON.parse(names);
                 var list = '';
                 
                 for(var i = 0; i < names.length; i++) {
@@ -55,6 +56,6 @@ submit.onclick = function (){
     };
     // Make the request
     
-    request.open('GET', 'http://rohitbhargava586.imad.hasura-app.io/counter', true);
+    request.open('GET', 'http://rohitbhargava586.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
