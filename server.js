ed0a5f4268bfb4eb2498c389daa6890362798f94
Rohit Bhargava <rohitbhargava586@gmail.com>
2017-08-23 22:52:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,54 +17,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-    'article-one': {
-        title: 'Article One | Rohit Bhargava',
-        heading: 'Article One',
-        date: 'Aug 11, 2017',
-        content: `
-                <p>
-                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.This is the content for my first article.
-                </p>
-                <p>
-                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.This is the content for my first article.
-                </p>
-                <p>
-                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.This is the content for my first article.'
-                </p>`    
-    },
-    'article-two': {
-        title: 'Article Two | Rohit Bhargava',
-        heading: 'Article Two',
-        date: 'Aug 13, 2017',
-        content: `
-                <p>
-                    This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my first article.This is the content for my first article.
-                </p>
-                <p>
-                    This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article.This is the content for my second article.
-                </p>
-                <p>
-                    This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my first article. This is the content for my second article.This is the content for my second article.'
-                </p>`  
-    },
-    'article-three': {
-        title: 'Article Three | Rohit Bhargava',
-        heading: 'Article Three',
-        date: 'Aug 15, 2017',
-        content: `
-                <p>
-                    This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article.This is the content for my third article.
-                </p>
-                <p>
-                    This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article.This is the content for my third article.
-                </p>
-                <p>
-                    This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article. This is the content for my third article.This is the content for my third article.
-                </p>`  
-    }
-};
-
 function createTemplate (data) {
     var title = data.title;
     var heading = data.heading;
