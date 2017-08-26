935aa63a3bf37cc361a234f78f35c9ef177bb8f4
Rohit Bhargava <rohitbhargava586@gmail.com>
2017-08-26 23:47:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/', function (req, res) {
 
 function hash(input, salt) {
     // How to create a hash
-    var hashed = crypto.pkdf2Sync(input, salt, 10000, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
 }
 
