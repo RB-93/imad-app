5c40b4293f569712952381cafab95876095203a4
Rohit Bhargava <rohitbhargava586@gmail.com>
2017-08-27 18:00:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,4 @@
-// Counter Code
-var button = document.getElementById('counter');
-
-button.onclick = function () {
-    // Create a request object
-    var request = new XMLHttpRequest();
-    
-    // Capture the response and stotre it in a variable.
-    request.onreadystatechange = function () {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            // Take some action
-            if(request.status === 200) {
-                var counter = request.responseText;
-                // Render the variable in correct span
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-        //not done yet
-    };
-    // Make the request
-    
-    request.open('GET', 'http://rohitbhargava586.imad.hasura-app.io/counter', true);
-    request.send(null);
-    
-};
-
-// Submit name
+// Submit username/password to login 
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function (){
@@ -39,22 +12,24 @@ submit.onclick = function (){
             // Take some action
             if(request.status === 200) {
                 // Capture a list of names and render it as a list.
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                
-                for(var i = 0; i < names.length; i++) {
-                    list += '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById('nameList');
-                ul.innerHTML = list;    
+                console.log('user logged in');
+                alert('Logged in sucessfully');
+            }
+            else if(request.status === 403) {
+                alert('Username/password in incorrect');
+            }
+            else if(request.status === 500) {
+                alert('Something wrong went on the server');
             }
         }
         //not done yet
     };
     // Make the request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET', 'http://rohitbhargava586.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    consloe.log(password);
+    request.open('POST', 'http://rohitbhargava586.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 };
