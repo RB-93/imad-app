9780a71265793e94a71de8c28241ea9e71dbb582
Rohit Bhargava <rohitbhargava586@gmail.com>
2017-08-27 18:52:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -148,6 +148,11 @@ app.get('/check-login', function (req, res) {
     }
 });
 
+app.get('/logout', function (req, res) {
+   delete req.session.auth;
+   res.send('Logged out');
+});
+
 var pool = new Pool(config);     // Pool ready
 app.get('/test-db', function (req, res) {
     // make select a request
