6f4ce4a56acc3de6121f2e24232c63e84d9decf7
Rohit Bhargava <rohitbhargava586@gmail.com>
2017-09-08 15:05:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.post('/create-user', function(req, res) {
            res.status(500).send(err.toString());
         }
         else {
-            if(result.rows.length === 0) {
+            if(username.value  === "" || password === "") {
                 //res.status(403).send('username/password is invalid');
                 
                 // For Android app MyBlog
