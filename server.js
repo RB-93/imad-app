ac836e0e4c7bead1a10d5b05ba9c73b092658aa2
Rohit Bhargava <rohitbhargava586@gmail.com>
2017-09-08 13:58:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.post('/login', function(req, res) {
    
    pool.query('SELECT * FROM "user" WHERE username=$1', [username], function(err, result) {
         if(err) {
-            res.status(500).send(JSON.parse('{err.toString()}'));
+            res.status(500).send(JSON.parse('{"error": err.toString()}'));
         }
         else {
             if(result.rows.length === 0) {
